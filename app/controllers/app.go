5caeb553344d6c6dbca3daf3c8fcab4a090c7911
stephan lindauer <stephanlindauer@posteo.de>
2019-03-21 19:10:37 +0100

    playing around with revel

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,6 +1,10 @@
 package controllers
 
 import (
+	"errors"
+	"net/http"
+
+	"github.com/criticalmaps/criticalmaps-janitor/app/models"
 	"github.com/revel/revel"
 )
 
@@ -9,5 +13,54 @@ type App struct {
 }
 
 func (c App) Index() revel.Result {
-	return c.Render()
+	gallery := []models.Gallery{}
+
+	result := DB.Select("id").Find(&gallery)
+	if result.Error != nil {
+		return c.RenderError(errors.New("Record Not Found"))
+	}
+
+	return c.Render(gallery)
+
+}
+
+func (c App) Thumbnail(id int) revel.Result {
+	var gallery models.Gallery
+
+	result := DB.Select("thumbnail").First(&gallery, id)
+	if result.Error != nil {
+		return c.RenderError(errors.New("Record Not Found"))
+	}
+
+	c.Response.WriteHeader(http.StatusOK, "image/jpeg")
+	c.Response.GetWriter().Write(gallery.Thumbnail)
+
+	return c.Result
+}
+
+func (c App) Image(id int) revel.Result {
+	var gallery models.Gallery
+
+	result := DB.Select("image").First(&gallery, id)
+	if result.Error != nil {
+		return c.RenderError(errors.New("Record Not Found"))
+	}
+
+	c.Response.WriteHeader(http.StatusOK, "image/jpeg")
+	c.Response.GetWriter().Write(gallery.Image)
+
+	return c.Result
+}
+
+func (c App) Delete(id int) revel.Result {
+	var imageToDelete models.Gallery
+
+	result := DB.First(&imageToDelete, id)
+	if result.Error != nil {
+		return c.RenderError(errors.New("Record Not Found"))
+	}
+
+	DB.Delete(&imageToDelete)
+
+	return c.RenderText("success")
 }
